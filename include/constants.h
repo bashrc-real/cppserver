fcec0fdf2481f3a2d04989c0970b79773d92df92
bashrc-real <saurabh8c@gmail.com>
2018-02-28 22:47:37 +0530

    Constants

---
+++ include/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS
+#define CONSTANTS
+
+namespace cppserver {
+    namespace constants{
+        const size_t buffer_length = 1024 * 4; // todo- benchmark
+        const string default_content_length_header = "Content-Length";
+        const string default_content_type_header = "Content-Type";
+        const string default_content_type = "text/html";
+    }
+}
